29808a88b212e635b38eb42a56c66bf85ae2209d
Bello18s <105251905+Bello18s@users.noreply.github.com>
2023-08-22 21:01:23 -0500

    Add files via upload

---
+++ Formulario/Formulario.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Formulario</title>
+</head>
+<body>
+    <form name="formulario" method="post"action="Prueba.php">
+
+        nombre: <input type="text" name="nombre" value ="">
+        Apellido: <input type="text" name="Apellido" value ="">
+        Documento: <input type="text" name="Documento" value ="">
+        Edad:  <input type="text" name="edad" value ="">
+
+        <input type="submit"/>
+
+
+    </form>
+    <?php
+    ?>
+</body>
+</html>
---
+++ Formulario/prueba.php
@@ -0,0 +1,10 @@
+<?php
+
+$nombre= $_POST['nombre'];
+$Apellido= $_POST['apellido'];
+$documento= $_POST['documento'];
+$edad= $_POST['edad'];
+
+echo 'Nombre:',$nombre,'Apellidpo:',$Apellido,'Apellido:',$documento,'Documento:',$edad,'edad';
+
+?>
